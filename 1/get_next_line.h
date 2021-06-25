6f6d5ab5b311bbcd04b1b99770daff06da2c23e1
hyungyoo <hjyoo901112@gmail.com>
2021-06-25 16:14:27 +0200

    gnl

---
+++ 1/get_next_line.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyungyoo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/21 12:05:18 by hyungyoo          #+#    #+#             */
+/*   Updated: 2021/01/23 14:53:17 by hyungyoo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+
+# ifndef OPEN_MAX
+#  define OPEN_MAX 32
+# endif
+
+size_t		ft_strlen(const char *str);
+size_t		ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t		ft_strlcat(char *dst, const char *src, size_t dstsize);
+char		*ft_strjoin(char *s1, char *s2);
+char		*ft_strdup(char *str);
+int			ft_is_new_line(char *backup);
+int			ft_split_line(char **backup, char **line, int count_index);
+int			ft_return_all(char **backup, char **line, int read_size);
+int			get_next_line(int fd, char **line);
+
+#endif
