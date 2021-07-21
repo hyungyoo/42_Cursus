e4390d3bb09c59a4551a7776e932ee24a387f07b
hyungyoo <hjyoo901112@gmail.com>
2021-07-21 15:27:10 +0200

    Makefile for checker

---
+++ Circle_03/Push_Swap/pushswap_files/ft_ajouter_node_split.c
@@ -0,0 +1,17 @@
+#include "../inc/push_swap.h"
+
+void	ft_ajouter_node_split(t_stack **stack, char *num)
+{
+	char	**num_split;
+	int		i;
+
+	i = 0;
+	num_split = ft_split(num);
+	while (num_split[i])
+	{
+		ft_ajouter_node(stack, ft_new_node(num_split[i]));
+		i++;
+	}
+	ft_free_split(num_split);
+	num_split = NULL;
+}
