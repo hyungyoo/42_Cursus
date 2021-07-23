195e2bc7f0692bafba87ee57ce95c31c8cea1aa8
hyungyoo <hjyoo901112@gmail.com>
2021-07-23 16:44:06 +0200

    finir projet push_swap

--- Circle_03/Rank02/GNL/GNL_01/get_next_line.c
+++ Circle_03/Rank02/GNL/GNL_01/get_next_line.c
@@ -1,6 +1,6 @@
 #include "get_next_line.h"
 
-int		get_next_line(char **line)
+int	get_next_line(char **line)
 {
 	int		i;
 	int		l;
@@ -10,7 +10,8 @@ int		get_next_line(char **line)
 
 	r = 0;
 	l = 1;
-	if (!(*line = malloc(l)))
+	*line = malloc(l);
+	if (!*line)
 		return (-1);
 	(*line)[0] = 0;
 	while ((r = read(0, &c, 1)) && l++ && c != '\n')
@@ -21,7 +22,7 @@ int		get_next_line(char **line)
 			return (-1);
 		}
 		i = -1;
-		while (++i < l -2)
+		while (++i < l - 2)
 			tmp[i] = (*line)[i];
 		tmp[i] = c;
 		tmp [i + 1] = 0;
