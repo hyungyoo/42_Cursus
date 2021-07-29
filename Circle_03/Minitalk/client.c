6d44591dc4d9ff778cfeb6235644c349af89c67f
hyungyoo <hjyoo901112@gmail.com>
2021-07-29 03:02:15 +0200

    corriger split, et minitalk

---
+++ Circle_03/Minitalk/client.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int	main(void)
+{
+	printf("test pour client\n");
+	return (0);
+}
---
+++ Circle_03/Minitalk/server.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int		main(void)
+{
+	printf("test pour server\n");
+	return (0);
+}
