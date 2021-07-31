6557fedd2c6bb1976f7dc9c7156a7da8e1f05407
hyungyoo <hjyoo901112@gmail.com>
2021-07-31 21:42:34 +0200

    so_long, put pixel

--- Circle_03/So_long/main.c
+++ Circle_03/So_long/main.c
@@ -1,7 +1,41 @@
-#include <stdio.h>
+#include "so_long.h"
+
+typedef struct	s_data
+{
+	void	*img;
+	char	*addr;
+	int		bit_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char *dst;
+
+	dst = data->addr + (y *data->line_length + x * (data->bit_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
 
 int	main(void)
 {
+	void	*mlx;
+	void	*mlx_win;
+	t_data	data;
+
+	int	i = 0;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 500, 500, "test");
+	data.img = mlx_new_image(mlx, 500, 500);
+	data.addr = mlx_get_data_addr(data.img, &data.bit_per_pixel, &data.line_length, &data.endian);
+	while (i < 500)
+	{
+		my_mlx_pixel_put(&data, i, i, 0x00FF0000);
+		i++;
+	}
+	mlx_put_image_to_window(mlx, mlx_win, data.img, 0, 0);
+	mlx_loop(mlx);
 	printf("haha\n");
 	return (0);
 }
---
+++ Circle_03/So_long/so_long.h
@@ -0,0 +1,9 @@
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "mlx/mlx.h"
+
+#endif
