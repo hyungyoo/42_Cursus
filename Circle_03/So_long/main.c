4ea36ad41a6209d3475e0f9f1f2449d9e45722d9
hyungyoo <hjyoo901112@gmail.com>
2021-08-03 13:25:57 +0200

    concevoir le projet

--- Circle_03/So_long/main.c
+++ Circle_03/So_long/main.c
@@ -2,5 +2,30 @@
 
 int	main(void)
 {
+	// file open
+
+	// check map and parsing
+	// for check- rect, wall, 5 elements
+	
+	// create ptr_mlx and win (with pixel_size * rows and cols of map)
+
+
+	// create image of xpm
+
+	// change xpm of p, c, e
+
+	// main_loop
+	/*
+	* render 1, 0, e, p, c ->each put xpm
+	*/
+	
+
+	// key and mouse event
+	/*
+	* key event
+	* update
+	*/
+
+	// loop
 	return (0);
 }
--- Circle_03/So_long/so_long.h
+++ Circle_03/So_long/so_long.h
@@ -1,9 +1,28 @@
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
+#define BUFFER_SIZE 1
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include "mlx/mlx.h"
+
+/*
+* get_next_line.c
+*/
+int			ft_line(char *str, char **line, int ret);
+char		*ft_rest(char *str);
+char		*ft_read_line(char *str, int fd, int *ret);
+int			get_next_line(int fd, char **line);
+
+/*
+* get_next_line_utils.c
+*/
+size_t		ft_strlen(const char *str);
+char		*ft_strdup(const char *src);
+char		*ft_strjoin(char const *s1, char const *s2);
+int			ft_strchr(const char *s, int c);
+char		*ft_substr(char const *s, unsigned int start, size_t len);
 
 #endif
