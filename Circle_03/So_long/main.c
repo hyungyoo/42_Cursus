44b9449ce5c401318f36d0f346b61eac83750550
hyungyoo <hjyoo901112@gmail.com>
2021-08-04 08:22:52 +0200

    for xpm image and xpm to window

--- Circle_03/So_long/main.c
+++ Circle_03/So_long/main.c
@@ -6,7 +6,7 @@
 /*   By: hyungyoo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/03 14:31:57 by hyungyoo          #+#    #+#             */
-/*   Updated: 2021/08/04 07:59:32 by hyungyoo         ###   ########.fr       */
+/*   Updated: 2021/08/04 08:22:31 by hyungyoo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,10 @@ int	main(int argc, char **argv)
 	ft_map(argv[1], &all);
 	all.mlx.mlx = mlx_init();
 	all.mlx.win_mlx = mlx_new_window(all.mlx.mlx, all.mlx.window_width, all.mlx.window_height, "So_Long");
+	
+	//////mlx_xpm_file_to_imgae, mlx_get_data_addr/////
+	//ft_xpm_to_image(&(all.mlx));
+	///////////////////////////////////////////////////
 
 	// create ptr_mlx and win (with pixel_size * rows and cols of map)
 
