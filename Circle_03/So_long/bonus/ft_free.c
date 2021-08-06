735dcfdff9bd492a474d21f8d72f91de7c870be1
hyungyoo <hjyoo901112@gmail.com>
2021-08-06 22:48:47 +0200

    for norminette

--- Circle_03/So_long/bonus/ft_free.c
+++ Circle_03/So_long/bonus/ft_free.c
@@ -10,17 +10,4 @@ void	ft_free(t_info *all)
 	free(all->map.map_array);
 	close(all->map.fd);
 	close(all->map.fd2);
-	mlx_destroy_image(all->mlx.mlx, all->wall.img);
-	mlx_destroy_image(all->mlx.mlx, all->floor.img);
-	mlx_destroy_image(all->mlx.mlx, all->player.img);
-	mlx_destroy_image(all->mlx.mlx, all->collectible.img);
-	mlx_destroy_image(all->mlx.mlx, all->player_exit.img);
-	mlx_destroy_window(all->mlx.mlx, all->mlx.win_mlx);
-	if (all->enemy0.img)
-	{
-		mlx_destroy_image(all->mlx.mlx, all->enemy0.img);
-		mlx_destroy_image(all->mlx.mlx, all->enemy1.img);
-		mlx_destroy_image(all->mlx.mlx, all->enemy2.img);
-		mlx_destroy_image(all->mlx.mlx, all->enemy3.img);
-	}
 }
--- Circle_03/So_long/src/ft_free.c
+++ Circle_03/So_long/src/ft_free.c
@@ -10,10 +10,4 @@ void	ft_free(t_info *all)
 	free(all->map.map_array);
 	close(all->map.fd);
 	close(all->map.fd2);
-	mlx_destroy_image(all->mlx.mlx, all->wall.img);
-	mlx_destroy_image(all->mlx.mlx, all->floor.img);
-	mlx_destroy_image(all->mlx.mlx, all->player.img);
-	mlx_destroy_image(all->mlx.mlx, all->collectible.img);
-	mlx_destroy_image(all->mlx.mlx, all->player_exit.img);
-	mlx_destroy_window(all->mlx.mlx, all->mlx.win_mlx);
 }
