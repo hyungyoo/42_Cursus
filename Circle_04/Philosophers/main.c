603000b9da347392c8ae815ed92c78102a941b15
hyungyoo <hjyoo901112@gmail.com>
2021-08-18 16:27:55 +0200

    makefile, header_file, gettimeofday

---
+++ Circle_04/Philosophers/main.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyungyoo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/18 15:42:05 by hyungyoo          #+#    #+#             */
+/*   Updated: 2021/08/18 16:27:30 by hyungyoo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int main(void)
+{
+        struct timeval	bgn;
+        struct timeval	end;
+        
+		int				diff;
+        
+		gettimeofday(&bgn, NULL);
+        
+        
+		gettimeofday(&end, NULL);
+        
+        
+		return 0;
+}
---
+++ Circle_04/Philosophers/philo.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyungyoo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/18 15:43:32 by hyungyoo          #+#    #+#             */
+/*   Updated: 2021/08/18 15:47:40 by hyungyoo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <string.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+#endif
