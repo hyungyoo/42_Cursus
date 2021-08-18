bd5f761f0e9758597267c019bb58e8f534fd6f24
hyungyoo <hjyoo901112@gmail.com>
2021-08-18 16:51:47 +0200

    pour timeofday function

--- Circle_04/Philosophers/main.c
+++ Circle_04/Philosophers/main.c
@@ -6,7 +6,7 @@
 /*   By: hyungyoo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/18 15:42:05 by hyungyoo          #+#    #+#             */
-/*   Updated: 2021/08/18 16:27:30 by hyungyoo         ###   ########.fr       */
+/*   Updated: 2021/08/18 16:51:06 by hyungyoo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,24 @@
 
 int main(void)
 {
-        struct timeval	bgn;
-        struct timeval	end;
+        struct timeval	now;
+		long	now_int;
         
-		int				diff;
-        
-		gettimeofday(&bgn, NULL);
-        
-        
-		gettimeofday(&end, NULL);
-        
-        
+		gettimeofday(&now, NULL);
+		now_int = (now.tv_sec * 1000) + (now.tv_usec / 1000);
+		printf("%ld\n", now_int);
+		
+		gettimeofday(&now, NULL);
+		now_int = (now.tv_sec * 1000) + (now.tv_usec / 1000);
+		printf("%ld\n", now_int);
+		
+		gettimeofday(&now, NULL);
+		now_int = (now.tv_sec * 1000) + (now.tv_usec / 1000);
+		printf("%ld\n", now_int);
+		
+		gettimeofday(&now, NULL);
+		now_int = (now.tv_sec * 1000) + (now.tv_usec / 1000);
+		printf("%ld\n", now_int);
+		
 		return 0;
 }
