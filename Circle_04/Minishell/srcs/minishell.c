b933453ae43135456ec5160b5fee98ff2d99e570
hyungyoo <hjyoo901112@gmail.com>
2021-10-04 16:38:36 +0200

    enlever .swp

--- Circle_04/Minishell/srcs/minishell.c
+++ Circle_04/Minishell/srcs/minishell.c
@@ -6,11 +6,25 @@
 /*   By: hyungyoo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/30 17:01:30 by hyungyoo          #+#    #+#             */
-/*   Updated: 2021/10/01 03:25:30 by hyungyoo         ###   ########.fr       */
+/*   Updated: 2021/10/04 16:28:21 by hyungyoo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+
+void	minishell(char *str)
+{
+    str = readline("minishell $>");
+	if (str == NULL || (ft_strcmp(str, "exit") == 0))
+	{
+		ft_putstr_fd("minishell exit\n", 1);
+		exit (1);
+	}
+	add_history(str);
+	parsing(str);
+	free(str);
+	str = NULL;
+}
 
 int	main(int ac, char **av, char **env)
 {
@@ -26,16 +40,7 @@ int	main(int ac, char **av, char **env)
     {   
 		signal(SIGINT, handler);
 		signal(SIGQUIT, handler);
-        str = readline("minishell $>");
-		if (str == NULL || (ft_strcmp(str, "exit") == 0))
-		{
-			ft_putstr_fd("minishell exit\n", 1);
-			exit (1);
-		}
-        add_history(str);
-		parsing(str);
-		free(str);
-		str = NULL;
+		minishell(str);
 	}
 	return (0);
 }
