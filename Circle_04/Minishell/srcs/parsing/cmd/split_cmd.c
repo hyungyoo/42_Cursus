79591fc89027ce7b3a25686a36c2334f366257a3
hyungyoo <hjyoo901112@gmail.com>
2021-10-10 16:46:33 +0200

    pour split_cmd mais, 2 leaks

--- Circle_04/Minishell/srcs/parsing/cmd/split_cmd.c
+++ Circle_04/Minishell/srcs/parsing/cmd/split_cmd.c
@@ -6,7 +6,7 @@
 /*   By: hyungyoo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/08 22:14:47 by hyungyoo          #+#    #+#             */
-/*   Updated: 2021/10/09 19:46:06 by hyungyoo         ###   ########.fr       */
+/*   Updated: 2021/10/10 16:45:53 by hyungyoo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,19 +25,6 @@ int	ft_strcmp_pivot(char *str)
 	else if (!ft_strcmp(str, ">>"))
 		return (1);
 	return (0);
-}
-
-char	*ft_strjoin_free(char *s1, char *s2)
-{
-	char	*ret;
-	char	*tmp;
-
-	tmp = ft_strjoin(s1, " ");
-	free(s1);
-	ret = ft_strjoin(tmp, s2);
-	free(s2);
-	free(tmp);
-	return (ret);
 }
 
 int	ft_count_pivot(char **str)
@@ -63,60 +50,8 @@ char	**ft_malloc_split(int	size_split)
 	ret = (char **)malloc(sizeof(char*) * (size_split) + 1);
 	if (!ret)
 		return (NULL);
-	ret[size_split] = '\0';
+	ret[size_split] = NULL;
 	return (ret);
-}
-
-char	**ft_split_cmd(char *str)
-{
-	char	**ret;
-	char	**split_str;
-	int		size_split;
-	int		i;
-	int		index_ret;
-
-	split_str = ft_split(str, ' ');
-	size_split = ft_count_pivot(split_str) + 1;
-	ret = ft_malloc_split(size_split);
-	i = 0;
-	index_ret = 0;
-	while (split_str[i])
-	{
-		/*
-		   to do:
-		   1. split_str[i] == pivot,
-		   strjoin et 
-		   changement de ret[index_ret++] et encore strjoin
-		 */
-		i++;
-	}
-	return (ret);
-}
-			
-		
-	/*
-	   if (str[i] == mt_cmd ou str[i + 1] == mt_cmd)
-	  		==> on doit finir 
-	*/
-	/*
-	to do
-		1. num_split -- > malloc double_array  ok
-		2. get_strjoin for each multiple_cmd
-			cat |>infile |
-			grep hello > 
-			cat outfile <
-			heool
-		3. ou bien, donne justement addresse!
-	*/
-
-
-void	ft_print_all(char **ret)
-{
-	int	i = 0;
-	if (!ret)
-		return ;
-	while (ret[i])
-		printf("%s\n", ret[i++]);
 }
 
 void	ft_free_double(char **str)
@@ -130,63 +65,121 @@ void	ft_free_double(char **str)
 	free(str);
 }
 
+char	*ft_strjoin_free(char *s1, char *s2)
+{
+	char	*ret;
+	char	*tmp;
+
+	tmp= ft_strjoin(s1, " ");
+	ret = ft_strjoin(tmp, s2);
+	free(tmp);
+	free(s2);
+	free(s1);
+	return (ret);
+}
+
+char	**ft_split_cmd(char *str_for_split)
+{
+	char	**ret;
+	char	**str;
+	int		size_split;
+	int		i;
+	int		ret_index;
+
+	str = ft_split(str_for_split, ' ');
+	size_split = ft_count_pivot(str) + 1;
+	ret = ft_malloc_split(size_split);
+	i = 0;
+	ret_index = 0;
+	while (str[i])
+	{
+		if (i == 0 && ft_strcmp_pivot(str[0]))
+		{
+			ret[ret_index++] = strdup(str[0]);
+			free(str[0]);
+			i++;
+		}
+		else
+		{
+			ret[ret_index] = strdup(str[i]);
+			free(str[i++]);
+			while (str[i] && !ft_strcmp_pivot(str[i]))
+				ret[ret_index] = ft_strjoin_free(ret[ret_index], str[i++]);
+			if (str[i] && ft_strcmp_pivot(str[i]))
+				ret[ret_index] = ft_strjoin_free(ret[ret_index], str[i++]);
+			ret_index++;
+		}
+	}
+	return (ret);
+}
+
+void	ft_print_all(char **ret)
+{
+	int	i = 0;
+	if (!ret)
+		return ;
+	while (ret[i])
+		printf("%s\n", ret[i++]);
+}
+
 int	main(void)
 {
-	char str[100] = "cat |>infile | grep hello > cat outfile < heool";
-	char str2[100] = "< cat |>infile | grep hello > cat outfile < heool";
+	char str[100] = "cat |>infile | grep hello > cat outfile | grep hell";
+	char str2[100] = "< cat |>infile | grep hello > cat outfile < heool >";
 	char **ret = ft_split_cmd(str);
-	char **ret2 = ft_split_cmd(str);
+	char **ret2 = ft_split_cmd(str2);
 	
 	
-	printf("count_pivot = %d\n", ft_count_pivot(ft_split(str, ' ')));
 	printf("%s\n\n", str);
 	
-	printf("count_pivot2 = %d\n", ft_count_pivot(ft_split(str2, ' ')));
 	printf("%s\n\n", str2);
 	
+	printf("\n\n\n");
+	printf("ret \n");
 	ft_print_all(ret);	
+	printf("\n\n\n");
+	printf("ret2 \n");
+	ft_print_all(ret2);
 
+	ft_free_double(ret);
+	ft_free_double(ret2);
+	printf("\n\n\n");
 	return (0);
 }
 
 /*
+ * 도대체 
+ *
+ * Process:         a.out [93623]
+Path:            /Users/USER//a.out
+Load Address:    0x10cfb2000
+Identifier:      a.out
+Version:         ???
+Code Type:       X86-64
+Platform:        macOS
+Parent Process:  leaks [93622]
 
-	if 
+Date/Time:       2021-10-10 16:41:49.091 +0200
+Launch Time:     2021-10-10 16:41:48.653 +0200
+OS Version:      macOS 11.5.2 (20G95)
+Report Version:  7
+Analysis Tool:   /usr/bin/leaks
 
-	split multi cmd;
-	in there, multi cmd aussi! 
-	cmd1 option arg m_cmd cmd2 option arg..
-	
-	split오류 == 
-	pivot이 << >> 은 인식못함!
-	그렇기때문에, 좆됨...
-	DONC, avec strjoin, et avec strcmp(ret[i], < ou << ou | ou > ou >>);
-	avec cela, strjoin jusqu'a multi_cmd
-	아니면 check_pivot에 strcmp로 <<가 있는지 확인하는것도있는데 그건별로임..
-	그리고 cmd1| 이것도 pivot으로 읽을듯
-	다른방법을 찾아야함
-	다 읽어가면서 앞뒤가 space이며 pivot = "|" < << > >> 인 경우를 기준으로
-	split할수있는 함수가 있어야한다.
-	
-	!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-	아니면 전부다 띄어쓰기로 split한것을 합쳐보면서, 만약에 | << >> > 일떄는 그것까지 합치고
-	나머지는 free하는것도 방법인듯함
-	좋아!
+Physical footprint:         356K
+Physical footprint (peak):  356K
+----
 
-	이걸위해서는 add_split함수로 그안에있는것들을 <|<<>>>일때, 합치도록!
-	strjoin함수로!
+leaks Report Version: 4.0
+Process 93623: 170 nodes malloced for 12 KB
+Process 93623: 2 leaks for 192 total leaked bytes.
 
-	cmd1 option arg m_cmd
+    2 (192 bytes) ROOT LEAK: 0x7fba71405990 [96]
+       1 (96 bytes) 0x7fba71405a20 [96]
 
-	cmd2 option arg null
+make: *** [all] Error 1
 
-	
+어디인지?
 
-	comme ca, apres
-	link all node;
-	print all node for check!
-
-	add first node in global value for free
-	
-	or t_parsing *ft_parsing_cmd(char *str) for free in minishell fucntion
-	*/
+malloc으로 che* * 5를 한다음에
+각각을 strduop으로 해도되는지?
+*/
