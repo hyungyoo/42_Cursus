db2e0fd8cc62554f6f2d7c43811e134a6986c03d
hyungyoo <hjyoo901112@gmail.com>
2021-10-11 13:40:06 +0200

    valgrind

--- Circle_04/Minishell/libft/ft_split.c
+++ Circle_04/Minishell/libft/ft_split.c
@@ -3,73 +3,99 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: keulee <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: hyungyoo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/05/17 18:20:39 by keulee            #+#    #+#             */
-/*   Updated: 2021/05/17 18:20:40 by keulee           ###   ########.fr       */
+/*   Created: 2021/10/11 13:39:41 by hyungyoo          #+#    #+#             */
+/*   Updated: 2021/10/11 13:39:43 by hyungyoo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strnum(char *s, char c)
+static char			**ft_malloc_error(char **tab)
 {
-	size_t	i;
-	size_t	num;
+	unsigned int	i;
 
-	num = 0;
 	i = 0;
+	while (tab[i])
+	{
+		free(tab[i]);
+		i++;
+	}
+	free(tab);
+	return (NULL);
+}
+
+static unsigned int	ft_get_nb_strs(char const *s, char c)
+{
+	unsigned int	i;
+	unsigned int	nb_strs;
+
+	if (!s[0])
+		return (0);
+	i = 0;
+	nb_strs = 0;
+	while (s[i] && s[i] == c)
+		i++;
 	while (s[i])
 	{
-		if (s[i] != c)
+		if (s[i] == c)
 		{
-			while (s[i] != c && s[i])
+			nb_strs++;
+			while (s[i] && s[i] == c)
 				i++;
-			num++;
+			continue ;
 		}
-		else
-			i++;
+		i++;
 	}
-	return (num);
+	if (s[i - 1] != c)
+		nb_strs++;
+	return (nb_strs);
 }
 
-void	str_malloc_copy(char *s, char c, size_t num, char **res)
+static void			ft_get_next_str(char **next_str, unsigned int *next_str_len,
+					char c)
 {
-	size_t	x;
-	size_t	i;
-	size_t	len;
+	unsigned int i;
 
+	*next_str += *next_str_len;
+	*next_str_len = 0;
 	i = 0;
-	x = 0;
-	while (s[i] && x < num)
+	while (**next_str && **next_str == c)
+		(*next_str)++;
+	while ((*next_str)[i])
 	{
-		len = 0;
-		if (s[i] != c)
-		{
-			while (s[i + len] != c && s[i + len])
-				len++;
-			res[x] = ft_calloc(sizeof(char), len + 1);
-			if (res[x] == 0)
-				return ;
-			ft_strlcpy(res[x], s + i, len + 1);
-			x++;
-		}
-		i += 1 + len;
+		if ((*next_str)[i] == c)
+			return ;
+		(*next_str_len)++;
+		i++;
 	}
 }
 
-char	**ft_split(char const *s, char c)
+char				**ft_split(char const *s, char c)
 {
-	size_t	num;
-	char	**res;
+	char			**tab;
+	char			*next_str;
+	unsigned int	next_str_len;
+	unsigned int	nb_strs;
+	unsigned int	i;
 
-	if (s == 0)
-		return (0);
-	num = ft_strnum((char *)s, c);
-	res = ft_calloc(sizeof(char *), num + 1);
-	if (res == 0)
-		return (0);
-	res[num] = 0;
-	str_malloc_copy((char *)s, c, num, res);
-	return (res);
+	if (!s)
+		return (NULL);
+	nb_strs = ft_get_nb_strs(s, c);
+	if (!(tab = (char **)malloc(sizeof(char *) * (nb_strs + 1))))
+		return (NULL);
+	i = 0;
+	next_str = (char *)s;
+	next_str_len = 0;
+	while (i < nb_strs)
+	{
+		ft_get_next_str(&next_str, &next_str_len, c);
+		if (!(tab[i] = (char *)malloc(sizeof(char) * (next_str_len + 1))))
+			return (ft_malloc_error(tab));
+		ft_strlcpy(tab[i], next_str, next_str_len + 1);
+		i++;
+	}
+	tab[i] = NULL;
+	return (tab);
 }
--- Circle_04/Minishell/srcs/parsing/cmd/cmd.c
+++ Circle_04/Minishell/srcs/parsing/cmd/cmd.c
@@ -6,7 +6,7 @@
 /*   By: hyungyoo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/06 18:26:07 by hyungyoo          #+#    #+#             */
-/*   Updated: 2021/10/11 12:34:58 by hyungyoo         ###   ########.fr       */
+/*   Updated: 2021/10/11 13:12:23 by hyungyoo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,8 +65,10 @@ t_parsing	*ft_newnode(char *str)
 void	ft_parsing_cmd(char *str)
 {
 	char **split_cmd;
-	
-	split_cmd = ft_split_cmd(ft_split(str, ' '));
+	char	**split_str;
+
+	split_str = ft_split(str, ' ');
+	split_cmd = ft_split_cmd(split_str);
 
 	int	i = 0;
 	while (split_cmd[i])
@@ -74,38 +76,7 @@ void	ft_parsing_cmd(char *str)
 	
 	ft_free_double(split_cmd);
 }
-
-	/*
-
-	if multi_cmd, parsing_ node
-
-	split ->  | < << > >> double array et multiple_cmd
-	if cat infile | grep "lol"
-		1er == cmd
-		2eme == commence avec - == option
-				sinon, arg
-
-
-		//////////autre fichier
-		3. link node (double)
-		4. ajouter global pour ft_exit free
-
-
-	if 
-
-	split multi cmd;
-	in there, multi cmd aussi! 
-	cmd1 option arg m_cmd cmd2 option arg..
-	
-	split오류 == 
-	pivot이 << >> 은 인식못함!
-	그렇기때문에, 좆됨...
-	아니면 check_pivot에 strcmp로 <<가 있는지 확인하는것도있는데 그건별로임..
-	그리고 cmd1| 이것도 pivot으로 읽을듯
-	다른방법을 찾아야함
-	다 읽어가면서 앞뒤가 space이며 pivot = "|" < << > >> 인 경우를 기준으로
-	split할수있는 함수가 있어야한다.
-	
+/*
 	!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 	아니면 전부다 띄어쓰기로 split한것을 합쳐보면서, 만약에 | << >> > 일떄는 그것까지 합치고
 	나머지는 free하는것도 방법인듯함
