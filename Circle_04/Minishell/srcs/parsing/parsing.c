7c7d9bf751b0a1b3a0dc433a9d1e9e4bcd44882c
hyungyoo <hjyoo901112@gmail.com>
2021-10-11 14:27:27 +0200

    quote doit etre avec echo function

--- Circle_04/Minishell/srcs/parsing/parsing.c
+++ Circle_04/Minishell/srcs/parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: hyungyoo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/30 17:06:00 by hyungyoo          #+#    #+#             */
-/*   Updated: 2021/10/11 12:22:24 by hyungyoo         ###   ########.fr       */
+/*   Updated: 2021/10/11 14:26:55 by hyungyoo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,18 @@
 // ft_verifier_dquote 리턴값 1이면, 오류
 void	parsing(char *str)
 {
-	if (ft_verifier_dquote(str))
-		return ;
 	ft_parsing_cmd(str);
+	/*
+	   this is for echo function
+	 */
+	
+	
+	//if (ft_verifier_dquote(str))
+	//	return ;
+	
+	
+	/*
+	   if quote, str doit etre ne pas considere
+
+	*/
 }
