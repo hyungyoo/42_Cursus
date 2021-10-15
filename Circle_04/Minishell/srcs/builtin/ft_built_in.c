07975f1b91289641567c5668f1e4c8ede27ade95
hyungyoo <hjyoo901112@gmail.com>
2021-10-15 17:07:59 +0200

    update to do

--- Circle_04/Minishell/srcs/builtin/ft_built_in.c
+++ Circle_04/Minishell/srcs/builtin/ft_built_in.c
@@ -6,11 +6,16 @@
 /*   By: hyungyoo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/15 16:31:21 by hyungyoo          #+#    #+#             */
-/*   Updated: 2021/10/15 16:44:09 by hyungyoo         ###   ########.fr       */
+/*   Updated: 2021/10/15 17:07:50 by hyungyoo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
+
+//void	ft_pass_arg(t_node **cmd);
+//to do:
+//	1. passe arg until next pipe ou redir
+//	2. and check il y a arg?
 
 void	ft_built_in(t_node	**cmd)
 {
