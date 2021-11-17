d053ee812fc1b5d592ce75b237c008a55e058d55
hyungyoo <hjyoo901112@gmail.com>
2021-11-17 06:05:49 +0100

    update README for fd

--- Circle_04/Minishell/README.md
+++ Circle_04/Minishell/README.md
@@ -1,13 +1,9 @@
 
-$숫자 
+1. ls | << end
+ls가 heredoc의 인자로 들어간다.
 
-ls | << end
+2. >file | ls // < Makefile cat > file | ls
+    file에 ls의 출력값이 들어간다.
 
->file | ls
-
-< Makefile  | cat
-
-< Makefile > file | ls
---> cat file ==> ls list
-
-
+3. < Makefile  | cat
+    cat에 Makefile값이 출력된다.
--- Circle_04/Minishell/srcs/execute_pipe.c
+++ Circle_04/Minishell/srcs/execute_pipe.c
@@ -20,49 +20,168 @@ void	execute_cmds_pipe(t_node **node, t_cmd *cmd)
         ft_execmd(*node, cmd);
 }
 
-int check_left(t_node *node)
+int	ft_left_fd_pipe(t_node **node, t_fd *fd)
 {
-    int ret_fd;
+	if (!(*node)->next || ((*node)->next && (*node)->next->type == PIPE))
+	{
+		ft_putstr_fd("minishell: syntax error ", 2);
+		ft_putstr_fd("near unexpected token 'newline'\n", 2);
+		return (0);
+	}
+	(*node) = (*node)->next;
+	fd->fd_in = open((*node)->str, O_RDONLY, 0644);
+	if (fd->fd_in == -1)
+	{
+		ft_error_message_left((*node)->str);
+		return (0);
+	}
+	dup2(fd->fd_in, 0);
+	return (1);
+}
 
-    ret_fd = 0;
-    while (node)
-    {
-        if (node->type == LEFT)
-            ret_fd = open(node->next->str, O_RDONLY, 0666);
-        if (node->next)
-            node = node->next;
-        else
-            break ;
-    }
-    return (ret_fd);
+void	heredoc_parent_pipe(t_fd *fd, int status)
+{
+	close(fd->fd_heredoc_pipe[1]);
+	dup2(fd->fd_heredoc_pipe[0], 0);
+	waitpid(g_info.pid_child, &status, 0);
+	g_info.pid_child = 0;
+	g_info.exit_code = WEXITSTATUS(status);
+}
+
+void	heredoc_child_pipe(t_fd *fd, t_cmd *cmd, t_node **node)
+{
+	char	*line;
+
+	close(fd->fd_heredoc_pipe[0]);
+	dup2(fd->fd_heredoc_pipe[1], 1);
+	ft_putstr_fd("> ", 2);
+	while (get_next_line(0, &line) > 0)
+	{
+		if (!ft_strcmp(line, (*node)->str))
+		{
+			free(line);
+			break ;
+		}
+		else
+			ft_putstr_fd("> ", 2);
+		ft_putstr_fd(line, 1);
+		ft_putstr_fd("\n", 1);
+		free(line);
+	}
+	ft_exit_minishell(0, &cmd);
+}
+
+int	ft_dleft_fd_pipe(t_node **node, t_fd *fd, t_cmd *cmd)
+{
+	int	status;
+
+	status = 0;
+	pipe(fd->fd_heredoc_pipe);
+	g_info.pid_child = fork();
+	if (!(*node)->next || ((*node)->next && (*node)->next->type == PIPE))
+	{
+		ft_putstr_fd("minishell: parse error near '\n'\n", 2);
+		return (0);
+	}
+	(*node) = (*node)->next;
+	if (g_info.pid_child > 0)
+		heredoc_parent_pipe(fd, status);
+	else if (g_info.pid_child == 0)
+		heredoc_child_pipe(fd, cmd, node);
+	return (1);
+}
+
+int	ft_right_fd_pipe(t_node **node, t_fd *fd)
+{
+	if (!(*node)->next || ((*node)->next && (*node)->next->type == PIPE))
+	{
+		ft_putstr_fd("minishell: syntax error ", 2);
+		ft_putstr_fd("near unexpected token 'newline'\n", 2);
+		return (0);
+	}
+	(*node) = (*node)->next;
+	fd->fd_out = open((*node)->str, O_CREAT | O_TRUNC | O_RDWR, 0644);
+	if (fd->fd_out == -1)
+		return (0);
+	dup2(fd->fd_out, 1);
+	return (1);
+}
+
+int	ft_dright_fd_pipe(t_node **node, t_fd *fd)
+{
+	if (!(*node)->next || ((*node)->next && (*node)->next->type == PIPE))
+	{
+		ft_putstr_fd("minishell: syntax error ", 2);
+		ft_putstr_fd("near unexpected token 'newline'\n", 2);
+		return (0);
+	}
+	(*node) = (*node)->next;
+	fd->fd_out = open((*node)->str, O_CREAT | O_APPEND | O_RDWR, 0644);
+	if (fd->fd_out == -1)
+		return (0);
+	dup2(fd->fd_out, 1);
+	return (1);
+}
+
+int	ft_fd_checker_pipe(t_node *node, t_fd *fd, t_cmd *cmd)
+{
+	t_node	*tmp;
+	int		flag_exit;
+
+	flag_exit = 1;
+	tmp = node->prev;
+	while (node != tmp && node->type != PIPE)
+	{
+		if (node->str && !strcmp(node->str, "<"))
+			flag_exit = ft_left_fd_pipe(&node, fd);
+		else if (node->str && !strcmp(node->str, "<<"))
+			flag_exit = ft_dleft_fd_pipe(&node, fd, cmd);
+		else if (node->str && !strcmp(node->str, ">"))
+			flag_exit = ft_right_fd_pipe(&node, fd);
+		else if (node->str && !strcmp(node->str, ">>"))
+			flag_exit = ft_dright_fd_pipe(&node, fd);
+		if (!flag_exit)
+			return (0);
+		if (node->next)
+			node = node->next;
+		else
+			break ;
+	}
+	return (1);
 }
 
 void    execute_pipe(t_node **node, t_cmd *cmd)
 {
 	int	status;
-    t_fd_pipe   fd;
+    t_fd_pipe   fd_pipe;
+    t_fd        fd;
 
-    pipe(fd.pipe_fd);
-	g_info.pid_child = fork();
-	if (g_info.pid_child == 0)
+    ft_set_fd(&fd);
+    if (ft_fd_checker_pipe(*node, &fd, cmd))
     {
-        close(fd.pipe_fd[0]);
-        dup2(fd.pipe_fd[1], 1);
-		execute_cmds_pipe(node, cmd);
-        close(fd.pipe_fd[1]);
-        ft_exit_minishell(g_info.exit_code, &cmd);
+        pipe(fd_pipe.pipe_fd);
+	    g_info.pid_child = fork();
+	    if (g_info.pid_child == 0)
+        {
+            close(fd_pipe.pipe_fd[0]);
+            dup2(fd_pipe.pipe_fd[1], 1);
+		    execute_cmds_pipe(node, cmd);
+            close(fd_pipe.pipe_fd[1]);
+            ft_exit_minishell(g_info.exit_code, &cmd);
+        }
+	    else if (g_info.pid_child > 0)
+	    {
+            close(fd_pipe.pipe_fd[1]);
+            dup2(fd_pipe.pipe_fd[0], 0);
+		    waitpid(g_info.pid_child, &status, 0);
+            close(fd_pipe.pipe_fd[0]);
+		    g_info.pid_child = 0;
+		    g_info.exit_code = WEXITSTATUS(status);
+            ft_move_to_last(node);
+            ft_update_last_env((*node)->str);
+	    }
+    //ft_close_fd(&fd);         // 지우면되는데, 이렇게되면 < Makefile | cat 또한 출력됨
     }
-	else if (g_info.pid_child > 0)
-	{
-        close(fd.pipe_fd[1]);
-        dup2(fd.pipe_fd[0], 0);
-		waitpid(g_info.pid_child, &status, 0);
-        close(fd.pipe_fd[0]);
-		g_info.pid_child = 0;
-		g_info.exit_code = WEXITSTATUS(status);
-        ft_move_to_last(node);
-        ft_update_last_env((*node)->str);
-	}
 }
 
 void    ft_exec_pipe(t_node *node, t_cmd *cmd)
