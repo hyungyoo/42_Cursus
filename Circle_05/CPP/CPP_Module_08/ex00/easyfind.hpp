40ce66a1732e0756ebb223fa3c2b811f6f55a81b
hyungyoo <hjyoo901112@gmail.com>
2022-01-29 23:08:07 +0100

    update

--- Circle_05/CPP/CPP_Module_08/ex00/easyfind.hpp
+++ Circle_05/CPP/CPP_Module_08/ex00/easyfind.hpp
@@ -12,6 +12,8 @@
 template<typename T>
 typename T::iterator	easyfind(T	&container, int value)
 {
+	// std::vector<T>::iterator is a dependent name, so you need a typename here to specify that it refers to a type. Otherwise it is assumed to refer to a non-type
+	// https://stackoverflow.com/questions/20934311/c-template-stdvectoriterator-error
 	typename T::iterator it = std::find(container.begin(), container.end(), value);
 	if (it == container.end())
 		throw std::runtime_error(std::string("not found"));
