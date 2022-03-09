16473742581c3db202872cdf4adac0e70e786a29
hyungyoo <hjyoo901112@gmail.com>
2022-03-09 20:10:50 +0100

    finir inception

--- Circle_06/Inception/srcs/requirements/mariadb/tools/setup_db.sh
+++ Circle_06/Inception/srcs/requirements/mariadb/tools/setup_db.sh
@@ -2,7 +2,6 @@
 
 service mysql start
 
-db_exist=$(echo "SHOW DATABASES" | mysql -u root | grep "wp_db" | wc -l)
 echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci" | mysql -u root
 echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'" | mysql -u root
 echo "GRANT ALL ON $MYSQL_DATABASE .* TO '$MYSQL_USER'@'%'" | mysql -u root
