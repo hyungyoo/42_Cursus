1faaeb9f970869b209b86ad7b47156238d96865d
hyungyoo <hjyoo901112@gmail.com>
2022-05-16 23:54:00 +0200

    for iterator

--- Circle_06/ft_containers/inc/random_access_iterator.hpp
+++ Circle_06/ft_containers/inc/random_access_iterator.hpp
@@ -6,7 +6,8 @@
 namespace ft {
 	
 	template <typename T>
-	class random_access_iterator {
+	class random_access_iterator 
+	{
 		public:
 			typedef	std::random_access_iterator_tag	iterator_category;
 			typedef	T								value_type;
@@ -20,11 +21,14 @@ namespace ft {
 			pointer	_ptr;
 		
 		public:
-			/* X a; X b(a); b = a; */
 			random_access_iterator(void) : _ptr(NULL) {}
+			
 			random_access_iterator(pointer ptr) : _ptr(ptr) {}
+			
 			random_access_iterator(const random_access_iterator &x) { *this = x; }
+			
 			operator random_access_iterator<value_type const>() const { return random_access_iterator<value_type const>(_ptr); }
+			
 			random_access_iterator &operator=(const random_access_iterator &x) {
 				if (this != &x)
 					_ptr = x._ptr;
@@ -32,10 +36,12 @@ namespace ft {
 			}
 			virtual ~random_access_iterator(void) {}
 
+			/*
 			pointer	get_ptr(void) const {
 				return (_ptr);
 			}
-			
+			*/
+
 			pointer base(void) const {
 				return (_ptr);
 			}
@@ -156,37 +162,37 @@ namespace ft {
 
 	template<class iter1, class iter2>
 	bool operator==(const random_access_iterator<iter1> &lhs, const random_access_iterator<iter2> &rhs) {
-		bool res = lhs.get_ptr() == rhs.get_ptr();
+		bool res = lhs.base() == rhs.base();
 		return (res);
 	}
 
 	template <class iter1, class iter2>
 	bool operator!=(const random_access_iterator<iter1> &lhs, const random_access_iterator<iter2> &rhs) {
-		bool res = lhs.get_ptr() != rhs.get_ptr();
+		bool res = lhs.base() != rhs.base();
 		return (res);
 	}
 
 	template <class iter1, class iter2>
 	bool operator<(const random_access_iterator<iter1> &lhs, const random_access_iterator<iter2> &rhs) {
-		bool res = lhs.get_ptr() < rhs.get_ptr();
+		bool res = lhs.base() < rhs.base();
 		return (res);
 	}
 
 	template <class iter1, class iter2>
 	bool operator<=(const random_access_iterator<iter1> &lhs, const random_access_iterator<iter2> &rhs) {
-		bool res = lhs.get_ptr() <= rhs.get_ptr();
+		bool res = lhs.base() <= rhs.base();
 		return (res);
 	}
 
 	template <class iter1, class iter2>
 	bool operator>(const random_access_iterator<iter1> &lhs, const random_access_iterator<iter2> &rhs) {
-		bool res = lhs.get_ptr() > rhs.get_ptr();
+		bool res = lhs.base() > rhs.base();
 		return (res);
 	}
 
 	template <class iter1, class iter2>
 	bool operator>=(const random_access_iterator<iter1> &lhs, const random_access_iterator<iter2> &rhs) {
-		bool res = lhs.get_ptr() >= rhs.get_ptr();
+		bool res = lhs.base() >= rhs.base();
 		return (res);
 	}
 }
